58a5c3ad16ff7fb9bc5b1be089a45bd937f34c3a
Linda <lkrueger17@gmail.com>
2024-10-05 08:58:14 +0200

    Change intersection types to utility types

---
+++ src/utility-types.spec.ts
@@ -0,0 +1,11 @@
+import {describe, expect, test} from '@jest/globals';
+
+/**
+ * https://www.typescriptlang.org/docs/handbook/utility-types.html
+ */
+
+describe('utility types', () => {
+    test('foo', () => {
+        expect(true).toBeTruthy()
+    });
+});
